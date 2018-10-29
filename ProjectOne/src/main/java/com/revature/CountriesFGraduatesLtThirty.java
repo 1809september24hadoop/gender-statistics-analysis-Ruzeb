1f20acaeea19d23de9aaafe5437b59066013211e
Ruzeb <ruzeb1996@gmail.com>
2018-10-29 08:59:17 -0400

    completed

--- ProjectOne/src/main/java/com/revature/CountriesFGraduatesLtThirty.java
+++ ProjectOne/src/main/java/com/revature/CountriesFGraduatesLtThirty.java
@@ -2,54 +2,70 @@ package com.revature;
 
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import static com.revature.util.Utility.*;
+
+/*
+ * MapReduce to identify countries where % of female graduates is less than 30.
+ * 
+ * Assumptions: 
+ * => Graduates are those who have graduated from first degree programmes (ISCED 6 & 7),
+ * => takes in three most recent percentages (if possible)
+ * 
+ * This MapReduce takes in LongWritable, Text and outputs Text, DoubleWritable
+ * Mapper shows each country's female graduates, 
+ * Reducer shows countries that have less than 30% female graduates
+ */
 public class CountriesFGraduatesLtThirty {
 	
-	public static class CountryFGraduatesMapper extends Mapper<Object, Text, Text, DoubleWritable>{
+	/*
+	 * Mapper that takes in LongWritable, Text
+	 * and outputs Text, DoubleWritable
+	 * Output data determines each country's female graduates based on assumptions
+	 */
+	public static class CountryFGraduatesMapper extends Mapper<LongWritable, Text, Text, DoubleWritable>{
 		
-		public static final String BACHELORS25 = "SE.TER.HIAT.BA.FE.ZS";
-		public static final String MASTERS25 = "SE.TER.HIAT.MS.FE.ZS";
-		public static final String DOCTORATES25 = "SE.TER.HIAT.DO.FE.ZS";
+		// constant for series codes pertaining to female graduates
+		public static final String TERTIARYGRAD = "SE.TER.CMPL.FE.ZS";
 		
-		
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
 			
-			String record = value.toString();
-			String recordTrim = record.substring(1, record.length()-2);
-			String[] columnSplit = recordTrim.split("\",\"");
-			//String[] columnSplit = record.split(",");
+			// parse the data by splitting on ","
+			String[] columnSplit = parsedInput(value);
 			
-			for (int index = 0; index < columnSplit.length; index++){
-				System.out.println(columnSplit[index] + " is at index " + index);
-			}
-			
-			if (columnSplit[3].equals(BACHELORS25) || columnSplit[3].equals(MASTERS25) || columnSplit[3].equals(DOCTORATES25)){
-				for (int index = 4; index < columnSplit.length; index++){
-					String data = columnSplit[index];
-					if (!data.equals("")){
-						context.write(new Text(columnSplit[0]), new DoubleWritable(Double.parseDouble(data)));
+			//map three most recent percentages (if possible)
+			if (columnSplit[3].equals(TERTIARYGRAD)){
+				for (int index = columnSplit.length - 1, counter = 3; counter > 0 && index > YEAR1960; index--){
+					if (!columnSplit[index].equals(EMPTY)){
+						context.write(new Text(columnSplit[0]), new DoubleWritable(Double.parseDouble(columnSplit[index])));
+						counter--;
 					}
 				}
-			}
+			}			
 		}	
+		
 	}
 	
-	public static class AverageCountryFGraduatesLtThirtyReducer extends Reducer<Text, DoubleWritable, Text, Boolean>{
+	/*
+	 * Reducer takes in Text, DoubleWritable
+	 * and outputs Text, DoubleWritable
+	 * Output identifies countries containing % female graduates less than 30 based on the assumptions
+	 */
+	public static class AverageCountryFGraduatesLtThirtyReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable>{
+		
+		@Override
 		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException{
-			System.out.println("HELLO REDUCER");
+
 			double totalPercentage = 0;
 			
 			int counter = 0;
@@ -59,17 +75,19 @@ public class CountriesFGraduatesLtThirty {
 			}
 			
 			totalPercentage /= counter;
-			if (totalPercentage < 30.0){
-				context.write(key, true);
-			}
 			
+			if (totalPercentage < 30.0){
+				context.write(key, new DoubleWritable(DoubleFormat(totalPercentage)));
+			}
 		}
+		
 	}
 	
 	public static void main(String[] args) throws Exception{
+		
 		Job job = new Job();
 		job.setJarByClass(CountriesFGraduatesLtThirty.class);
-		job.setJobName("Countries With female graduates < 30%");
+		job.setJobName("Countries with female graduates < 30%");
 		
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -81,7 +99,7 @@ public class CountriesFGraduatesLtThirty {
 		job.setReducerClass(AverageCountryFGraduatesLtThirtyReducer.class);
 		
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Boolean.class);
+		job.setOutputValueClass(DoubleWritable.class);
 
 		boolean success = job.waitForCompletion(true);
 		System.exit(success ? 0 : 1);
--- ProjectOne/src/main/java/com/revature/IncreaseInUSFemaleEducationFrom2000.java
+++ ProjectOne/src/main/java/com/revature/IncreaseInUSFemaleEducationFrom2000.java
@@ -1,112 +1,124 @@
 package com.revature;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.mapred.join.TupleWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import static com.revature.util.Utility.*;
 
+/*
+ * MapReduce to identify the average increase in US female education from 2000
+ * 
+ * Assumptions: 
+ * => Female education is synonymous with Primary and Secondary education
+ * 
+ * This MapReduce takes in LongWritable, Text and outputs Text, DoubleWritable
+ * Mapper outputs an ArrayWritable containing female education percentages from 2000 onwards
+ * for both Primary and Secondary education
+ * Reducer shows the average increase in both Primary and Secondary female education in the US from 2000
+ */
 public class IncreaseInUSFemaleEducationFrom2000 {
+	
+	/*
+	 * DoubleArrayWritable is a subclass of ArrayWritable
+	 */
+	public static class DoubleArrayWritable extends ArrayWritable{
+		public DoubleArrayWritable() {
+			super(DoubleWritable.class);
+		}
 
-	public static class USFemaleEducationFrom2000Mapper extends Mapper<Object, Text, Text, DoubleWritable>{
+	}
 
-		public static final String PRIMARYEDUCATIONFEMALES = "SE.PRM.NENR.FE";
+	/*
+	 * Mapper that takes in LongWritable, Text
+	 * and outputs Text, DoubleArrayWritable
+	 * Outputs ArrayWritable of DoubleWritables for each year from 2000 onwards
+	 * for US Female education in Primary and Secondary education
+	 */
+	public static class USFemaleEducationFrom2000Mapper extends Mapper<LongWritable, Text, Text, DoubleArrayWritable>{
 
+		public static final String PRIMARYENROLLMENTFEMALES = "SE.PRM.NENR.FE";
+		public static final String SECONDARYENROLLMENTFEMALES = "SE.SEC.NENR.FE";
 
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-			String record = value.toString();
-			String recordTrim = record.substring(1, record.length()-2);
-			String[] columnSplit = recordTrim.split("\",\"");
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
 			
-			if (columnSplit[1].equals("USA")){
-				if (columnSplit[3].equals(PRIMARYEDUCATIONFEMALES)){
-					System.out.println("HERE");
-					for (int index = 44; index < columnSplit.length; index++){
-						String data = columnSplit[index];
-						if (!data.equals("")){
-							context.write(new Text(columnSplit[0]), new DoubleWritable(Double.parseDouble(data)));
+			String[] columnSplit = parsedInput(value);
+			
+			if (columnSplit[1].equals(USA)){
+				if (columnSplit[3].equals(PRIMARYENROLLMENTFEMALES) || columnSplit[3].equals(SECONDARYENROLLMENTFEMALES)){
+					List<DoubleWritable> arrList = new ArrayList<>();
+					
+					for (int index = YEAR2000; index < columnSplit.length; index++){
+						if (!columnSplit[index].equals(EMPTY)){
+							arrList.add(new DoubleWritable(Double.parseDouble(columnSplit[index])));
 						}
 					}
+					DoubleArrayWritable arr = new DoubleArrayWritable();
+					arr.set(arrList.toArray(new DoubleWritable[arrList.size()]));
+					context.write(new Text(columnSplit[2]), arr);
 				}
 			}
-
-//			if (columnSplit[2].equals("USA")){
-//				if (columnSplit[3].equals(PRIMARYEDUCATIONFEMALES)){
-//					for (int index = 44, next = 45; index < columnSplit.length-1 || next < columnSplit.length-1; index++, next++){
-//						//Integer year = new Integer(2000);
-////						if (!columnSplit[index].trim().equals("")){
-////							context.write(new Text(columnSplit[0]), 
-////									new TupleWritable(new Writable[]{new IntWritable(year++), 
-////											new DoubleWritable(Double.parseDouble(columnSplit[index]))}));
-////						}
-//						if (!columnSplit[index].trim().equals("")){
-//							Double precedent = Double.parseDouble(columnSplit[index]);
-//							index++;
-//							while(!columnSplit[next].trim().equals("") && next < columnSplit.length-1){
-//								next++;
-//							}
-//							Double current = Double.parseDouble(columnSplit[index]);
-//							context.write(new Text(columnSplit[0]), new DoubleWritable(current - precedent));
-//						}
-//					}
-//				}
-//			}	
 		}	
 	}
 
-	public static class IncreaseInEducationReducer extends Reducer<Text, DoubleWritable, Text, Double>{
-		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException{
+	/*
+	 * Reducer takes in Text, DoubleArrayWritable
+	 * and outputs Text, DoubleWritable
+	 * Output identifies the average increase in US female education for 
+	 * Primary and Secondary education from 2000 onwards
+	 */
+	public static class IncreaseInEducationReducer extends Reducer<Text, DoubleArrayWritable, Text, DoubleWritable>{
+		
+		@Override
+		public void reduce(Text key, Iterable<DoubleArrayWritable> values, Context context) throws IOException, InterruptedException{
 			
-			double differences = 0.0;
-			double counter = 0.0;
+			double differences = 0.0;;
 			double average = 0.0;
-			double previous = 0.0;
 			
+			DoubleArrayWritable dubArr = new DoubleArrayWritable();
 			if (values.iterator().hasNext()){
-				previous = values.iterator().next().get();	
-			}
-			while (values.iterator().hasNext()){
-				double last = previous;
-				double next = values.iterator().next().get();
-				previous = next;
-				differences += last - next;
-				counter++;
+				dubArr = values.iterator().next();	
 			}
 			
-			average = differences / counter;
-			context.write(new Text("USA"), average);
+			Writable[] arr = dubArr.get();
 			
-//			for (DoubleWritable dr : values){
-//				context.write(new Text("USA"), dr.get());
-//			}
+			for (int index = 0; index + 1 < arr.length; index++){
+				differences += ((DoubleWritable)arr[index+1]).get() - ((DoubleWritable) arr[index]).get();
+			}
+			
+			average = differences / (arr.length - 1.0);
+			context.write(key, new DoubleWritable(DoubleFormat(average)));
+			
 		}
 	}
 
 	public static void main(String[] args) throws Exception{
 		Job job = new Job();
 		job.setJarByClass(IncreaseInUSFemaleEducationFrom2000.class);
-		job.setJobName("Countries With female graduates < 30%");
+		job.setJobName("Average Increase in USA Female Education from Year 2000");
 
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(DoubleWritable.class);
+		job.setMapOutputValueClass(DoubleArrayWritable.class);
 
 		job.setMapperClass(USFemaleEducationFrom2000Mapper.class);
 		job.setReducerClass(IncreaseInEducationReducer.class);
 
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Double.class);
+		job.setOutputValueClass(DoubleWritable.class);
 
 		boolean success = job.waitForCompletion(true);
 		System.exit(success ? 0 : 1);
